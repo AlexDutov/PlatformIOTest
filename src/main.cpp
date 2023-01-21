ad326b9d46bfa2af523d1fbd022d8128cb68170e
Alex Dutov <alex@example.com>
2023-01-21 21:41:11 +0200

    Init repo

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void setup() {
+  //Init the led
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+  Serial.println("Yohoho");
+
+}
